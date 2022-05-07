f98df06fc72af365a9e1e23d4b4822c3ed4969cc
Hossen <mdmitho199911@gmail.com>
2022-05-07 22:50:33 +0600

    contact section react font awesome done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,9 @@
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { faMobile } from "@fortawesome/free-solid-svg-icons";
+import { faMapLocation } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 
 const Contact = () => {
@@ -14,11 +18,22 @@ const Contact = () => {
             <div class="text-left italic mt-16 ml-20   ">
               <h5 class="text-gray-900 text-2xl font-medium mb-2">Contact Details</h5>
               <h1 className="text-gray-900 text-xl font-medium mb-2">Find Location</h1>
-              <h2>Bangladesh , Dhaka</h2>
+              <h2>
+                {" "}
+                <FontAwesomeIcon className="text-red-700 me-2" icon={faMapLocation} />
+                Bangladesh , Dhaka
+              </h2>
               <h1 className="text-gray-900 text-xl font-medium mb-2">Call Us 24/7</h1>
-              <h2>+880 1878573729</h2>
+              <h2>
+                {" "}
+                <FontAwesomeIcon className="text-red-700 me-2" icon={faMobile} />
+                +880 1878573729
+              </h2>
               <h1 className="text-gray-900 text-xl font-medium mb-2">Mail Us</h1>
-              <h2>mdmitho199911@gmail.com</h2>
+              <h2>
+                <FontAwesomeIcon className="text-red-700 me-2" icon={faEnvelope} />
+                mdmitho199911@gmail.com
+              </h2>
             </div>
           </div>
         </div>
